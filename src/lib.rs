d9c3a7ac10aaf652c67fc8b2dfa15d58708b2b43
Your Name <you@example.com>
2024-10-13 23:58:25 +0200

    adding compile error

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@ extern crate core as _;
 #[macro_use]
 extern crate std as _;
 
+#[cfg(not(target_has_atomic))]
+compile_error!("the current device does not have atomics");
+
 cfg_if! {
     if #[cfg(feature = "std")] {
         pub use ::std::sync::atomic::*;
